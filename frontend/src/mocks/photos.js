480a489ce084e18c7f496a4f8724cb6c51a0f185
rsemacte <reactsme3@gmail.com>
2023-05-15 10:24:09 -0400

    swapping mock data

---
+++ frontend/src/mocks/photos.js
@@ -0,0 +1,174 @@
+const photos = [
+  {
+  "id": "1",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
+  },
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`
+  },
+  "user": {
+    "id": "1",
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+  },{
+    "id": "2",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`
+    },
+    "user": {
+      "id": "2",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "3",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`
+    },
+    "user": {
+      "id": "3",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "4",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-4-Full.jpg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-4-Regular.jpg`
+    },
+    "user": {
+      "id": "4",
+      "updated_at": "2016-07-10T11:00:01-05:00",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "5",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`
+    },
+    "user": {
+      "id": "5",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "6",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-4-Full.jpg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-4-Regular.jpg`
+    },
+    "user": {
+      "id": "6",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "7",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`
+    },
+    "user": {
+      "id": "7",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "8",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-8-Full.jpg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-8-Regular.jpg`
+    },
+    "user": {
+      "id": "8",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "9",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`
+    },
+    "user": {
+      "id": "9",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  },
+  {
+    "id": "10",
+    "location": {
+      "city": "Montreal",
+      "country": "Canada"
+    },
+    "urls": {
+      "full": `${process.env.PUBLIC_URL}/Image-8-Full.jpg`,
+      "regular": `${process.env.PUBLIC_URL}/Image-8-Regular.jpg`
+    },
+    "user": {
+      "id": "10",
+      "username": "exampleuser",
+      "name": "Joe Example",
+      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    }
+  }
+]
+
+export default photos;
---
+++ frontend/src/mocks/topics.js
@@ -0,0 +1,29 @@
+const topics = [
+  {
+    "id": "1",
+    "slug": "topic-1",
+    "title": "Topic 1"
+  },  
+  {
+    "id": "2",
+    "slug": "topic-2",
+    "title": "Topic 2"
+  },
+  {
+    "id": "3",
+    "slug": "topic-3",
+    "title": "Topic 3"
+  },
+  {
+    "id": "4",
+    "slug": "topic-4",
+    "title": "Topic 4"
+  },  
+  {
+    "id": "5",
+    "slug": "topic-5",
+    "title": "Topic 5"
+  }
+];
+
+export default topics;
