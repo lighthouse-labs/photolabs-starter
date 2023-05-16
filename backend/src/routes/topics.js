444f42cfa627c769f008a8bdf15121503e616b04
rsemacte <reactsme3@gmail.com>
2023-05-15 21:46:16 -0400

    add similar photos query, update frontend mocks

--- backend/src/routes/topics.js
+++ backend/src/routes/topics.js
@@ -38,6 +38,32 @@ module.exports = db => {
           'location', json_build_object(
             'city', photo.city,
             'country', photo.country
+          ),
+          'similar_photos', (
+            SELECT 
+              json_agg(
+                json_build_object(
+                  'id', similar_photo.id,
+                  'urls', json_build_object(
+                    'full', concat('${serverUrl}/images/', similar_photo.full_url),
+                    'regular', concat('${serverUrl}/images/', similar_photo.regular_url)
+                  ),
+                  'user', json_build_object(
+                    'username', similar_user_account.username,
+                    'name', similar_user_account.fullname,
+                    'profile', concat('${serverUrl}/images/', similar_user_account.profile_url)
+                  ),
+                  'location', json_build_object(
+                    'city', similar_photo.city,
+                    'country', similar_photo.country
+                  )
+                )
+              )
+            FROM photo AS similar_photo
+            JOIN user_account AS similar_user_account ON similar_user_account.id = similar_photo.user_id
+            WHERE similar_photo.id <> photo.id
+            AND similar_photo.topic_id = photo.topic_id
+            LIMIT 4
           )
         )
       ) as topic_photo_data
--- frontend/src/mocks/photos.js
+++ frontend/src/mocks/photos.js
@@ -1,5 +1,4 @@
-const photos = [
-  {
+const photo1 = {
   "id": "1",
   "location": {
     "city": "Montreal",
@@ -15,158 +14,263 @@ const photos = [
     "name": "Joe Example",
     "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
   }
-  },{
+}
+
+const photo2 = {
+  "id": "2",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
+  },
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`
+  },
+  "user": {
     "id": "2",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`
-    },
-    "user": {
-      "id": "2",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo3 = {
+  "id": "3",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
   },
-  {
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`
+  },
+  "user": {
     "id": "3",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`
-    },
-    "user": {
-      "id": "3",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo4 = {
+  "id": "4",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
   },
-  {
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-4-Full.jpg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-4-Regular.jpg`
+  },
+  "user": {
     "id": "4",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-4-Full.jpg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-4-Regular.jpg`
-    },
-    "user": {
-      "id": "4",
-      "updated_at": "2016-07-10T11:00:01-05:00",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "updated_at": "2016-07-10T11:00:01-05:00",
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo5 = {
+  "id": "5",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
   },
-  {
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`
+  },
+  "user": {
     "id": "5",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`
-    },
-    "user": {
-      "id": "5",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo6 =  {
+  "id": "6",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
   },
-  {
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-4-Full.jpg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-4-Regular.jpg`
+  },
+  "user": {
     "id": "6",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-4-Full.jpg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-4-Regular.jpg`
-    },
-    "user": {
-      "id": "6",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo7 = {
+  "id": "7",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
   },
-  {
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`
+  },
+  "user": {
     "id": "7",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`
-    },
-    "user": {
-      "id": "7",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo8 = {
+  "id": "8",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
   },
-  {
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-8-Full.jpg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-8-Regular.jpg`
+  },
+  "user": {
     "id": "8",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-8-Full.jpg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-8-Regular.jpg`
-    },
-    "user": {
-      "id": "8",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo9 = {
+  "id": "9",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
   },
-  {
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`
+  },
+  "user": {
     "id": "9",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`
-    },
-    "user": {
-      "id": "9",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+const photo10 = {
+  "id": "10",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
+  },
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-8-Full.jpg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-8-Regular.jpg`
+  },
+  "user": {
+    "id": "10",
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+  }
+}
+
+
+
+
+const mockPhotos = [
+  {
+    ...photo1,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
     }
   },
   {
-    "id": "10",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-8-Full.jpg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-8-Regular.jpg`
-    },
-    "user": {
-      "id": "10",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+    ...photo2,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo3,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo4,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo5,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo6,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo7,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo8,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo9,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+    }
+  },
+  {
+    ...photo10,
+    similar_photos: {
+      photo2,
+      photo3,
+      photo4,
+      photo5,
     }
   }
 ]
