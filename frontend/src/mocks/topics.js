a25080c85a7d7903157f63232bdfbbcf49f9b135
rsemacte <reactsme3@gmail.com>
2023-05-24 10:42:59 -0400

    remove .json, add different topis

--- frontend/src/mocks/topics.js
+++ frontend/src/mocks/topics.js
@@ -2,27 +2,27 @@ const topics = [
   {
     "id": "1",
     "slug": "topic-1",
-    "title": "Topic 1"
+    "title": "Nature"
   },  
   {
     "id": "2",
     "slug": "topic-2",
-    "title": "Topic 2"
+    "title": "Travel"
   },
   {
     "id": "3",
     "slug": "topic-3",
-    "title": "Topic 3"
+    "title": "People"
   },
   {
     "id": "4",
     "slug": "topic-4",
-    "title": "Topic 4"
+    "title": "Fashion"
   },  
   {
     "id": "5",
     "slug": "topic-5",
-    "title": "Topic 5"
+    "title": "Animals"
   }
 ];
 
