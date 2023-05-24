dc6379ca8f0f8fca2e52536008fcd7c17ba30fdf
David Halpin <devhalpin86@gmail.com>
2023-05-24 14:59:51 -0700

    Add keys from .env

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,7 +1,11 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
+  host: process.env.PGHOST,
+  name: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
