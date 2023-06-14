e057da9d0de5a7b7f80f5287ef46d38173514781
Andy Lindsay <andydlindsay@gmail.com>
2023-06-14 13:56:58 -0700

    removes defaultProps

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import './TopicList.scss';
+import "./TopicList.scss";
+
+const topics = [
+  {
+    id: "1",
+    slug: "topic-1",
+    title: "Nature",
+  },
+  {
+    id: "2",
+    slug: "topic-2",
+    title: "Travel",
+  },
+  {
+    id: "3",
+    slug: "topic-3",
+    title: "People",
+  },
+];
 
 const TopicList = () => {
   <div className="top-nav-bar__topic-list">
     {/* Insert React */}
   </div>
-}
+};
 
-TopicList.defaultProps = {
-  topics: [
-    {
-      "id": "1",
-      "slug": "topic-1",
-      "title": "Nature"
-    },  
-    {
-      "id": "2",
-      "slug": "topic-2",
-      "title": "Travel"
-    },
-    {
-      "id": "3",
-      "slug": "topic-3",
-      "title": "People"
-    },
-  ]
-}
-export default TopicList
+export default TopicList;
