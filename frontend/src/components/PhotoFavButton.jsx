4720265f4473f4e4f900ade8ca54232605bfaea3
Mahdi Teimouri <mahditei@mail.com>
2023-06-14 16:06:22 -0700

    favicon modified

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,21 @@
-import React, { useCallback, useState } from 'react';
+import React from "react";
 
-import { FavIcon } from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import { FavIcon } from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  const { toggleFavorite, filled } = props;
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+    // The parent div has the onlick handler and not the child FavIcon.
+    <div className="photo-list--fav-icon" onClick={toggleFavorite}>
+      <div className="photo-list--fav-icon-svg">
+        {/* the color of the icon changes based on the filled state. */}
+        {/* the width of the outline changes based on the filled state. */}
+        <FavIcon
+          fill={filled === true ? "#C80000" : "#EEEEEE"}
+          outlineWidth={filled === true ? 4 : 2}
+        />
       </div>
     </div>
   );
