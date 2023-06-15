b5b707eb12fb54ed1c4afa3ecef8dbc9596fc0ff
Mahdi Teimouri <mahditei@mail.com>
2023-06-15 16:27:52 -0700

    topNavBarCreated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
+import TopNavigationBar from './components/TopNavigationBar';
 import './App.scss';
 import PhotoList from './components/PhotoList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const photos = Array.from({ length: 3 });
-
+  
+  <div className="App">
+     <TopNavigationBar />
+   </div>
+   
   return (
     <div>
       {photos.map((_, index) => (
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
+import "../styles/TopNavigationBar.scss";
 
-import '../styles/TopNavigationBar.scss'
+const TopNavigation = (props) => {
+  const refreshPage = () => {
+  };
 
-const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo" onClick={refreshPage}>
+        PhotoLabs
+      </span>
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
