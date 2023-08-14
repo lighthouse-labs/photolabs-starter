a5b4ad8ee2b72edea7fa4c04a9f72b359d7442a5
JacquelineMG <j.gilchrist@outlook.com>
2023-08-14 16:38:47 -0700

    implement rough like function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,18 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+
+  const [isFav, setIsFav] = useState(false);
+
+  const handleFavClick = ()=> { 
+    console.log(`set isFav as: ${!isFav}`);
+    {isFav === true ? setIsFav(false) : setIsFav(true)}
+  }
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={ handleFavClick }>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon fill={isFav === true ? "#C80000" : "#EEEEEE"} />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import FavIcon from "./FavIcon.jsx";
 import "../styles/PhotoListItem.scss";
+// import "../styles/PhotoFavButton.scss";
+import PhotoFavButton from "./PhotoFavButton.jsx";
+
 
 
 const PhotoListItem = (props) => {
 
   return (
-    <div className="photo-list__item">
-      
-        <img className="photo-list__image" alt="image" src={props.imageSource} />
+    <div className="photo-list__item">  
+      <PhotoFavButton />    
+      <img className="photo-list__image" alt="image" src={props.imageSource} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" alt="Profile Image" src={props.profile} />
          <span className="photo-list__user-info">
