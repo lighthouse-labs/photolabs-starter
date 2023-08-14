e6ec1f2ba84771fb21d26968e4920fc151803e83
JacquelineMG <j.gilchrist@outlook.com>
2023-08-14 15:09:54 -0700

    add map function to loop through data and display multiple PhotoListItems on the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,70 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const sampleDataForPhotoList = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    },
+    user: {
+      id: "1",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+  {
+    id: "2",
+    location: {
+      city: "Toronto",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+    },
+    user: {
+      id: "2",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+  {
+    id: "3",
+    location: {
+      city: "Ottawa",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    },
+    user: {
+      id: "3",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+];
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem 
-        key={sampleDataForPhotoListItem.id}
-        imageSource={sampleDataForPhotoListItem.imageSource} 
-        profile={sampleDataForPhotoListItem.profile}
-        username={sampleDataForPhotoListItem.username}
-        city={sampleDataForPhotoListItem.location.city}
-        country={sampleDataForPhotoListItem.location.country}
-        />
+      <PhotoList photoListData={sampleDataForPhotoList} />
+
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import FavIcon from "./FavIcon.jsx";
 import "../styles/PhotoListItem.scss";
 
 
@@ -7,11 +7,12 @@ const PhotoListItem = (props) => {
 
   return (
     <div className="photo-list__item">
+      
         <img className="photo-list__image" alt="image" src={props.imageSource} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" alt="Profile Image" src={props.profile} />
          <span className="photo-list__user-info">
-          {props.username} <br/>
+          {props.name} <br/>
           <span className="photo-list__user-location"> {props.city}, {props.country}  </span>
 
          </span>
