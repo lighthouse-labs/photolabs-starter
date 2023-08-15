0d50250026946066b0f6f10fde2bfbc3893b22cd
mattyu123 <matthewyu100@gmail.com>
2023-08-15 18:46:04 -0400

    style photoListItem component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
+//Sample data for the one photo list item
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+console.log(sampleDataForPhotoListItem)
+
 const App = () => {
+    
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem sampleImage={sampleDataForPhotoListItem}/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
 
@@ -14,8 +13,37 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  console.log("props here", props)
+  
+  return (
+    <div className="photo-list__item">
+      <img src={props.sampleImage.imageSource} className="photo-list__image"/>
+      <div className="photo-list__user-details">
+        <img src={props.sampleImage.profile} className="photo-list__user-profile photo-list__user-details"/>
+        <div>
+          <h3 className="photo-list__user-info">{props.sampleImage.username}</h3>
+          <h3 className="photo-list__user-location photo-list__user-info">{props.sampleImage.location.city}, {props.sampleImage.location.country}</h3>
+        </div>
+      </div>
+    </div>
+  )
 };
+
+
+
+//This below is okay, trying some stuff here
+// const PhotoListItem = (props) => {
+//   console.log("props here", props)
+  
+//   return (
+//     <div className="photo-list__item">
+//       <img src={props.sampleImage.imageSource} className="photo-list__image"/>
+//       <img src={props.sampleImage.profile} className="photo-list__user-profile photo-list__user-details"/>
+//       <h3 className="photo-list__user-info photo-list__user-details">{props.sampleImage.username}</h3>
+//       <h3 className="photo-list__user-location photo-list__user-info">{props.sampleImage.location.city}, {props.sampleImage.location.country}</h3>
+//     </div>
+//   )
+// };
 
 export default PhotoListItem;
