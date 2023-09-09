551d1b79bde8763467e59f8c77e4e6c09a9b16f3
ralph <ralphdcouto1987@gmail.com>
2023-09-08 23:06:03 -0400

    building a component using sample data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
+
 const sampleDataForPhotoListItem = {
   id: "1",
   location: {
@@ -14,8 +15,21 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({username, imageSource, id, location, profile}) => { 
+  return (
+  <div className="photo-list-item">
+<div className="photo-username">
+<img src={imageSource} alt={`photo ${id}`} className="main-photo" />
+<img src={profile} alt={`${username} profile`} className="profile-photo" />
+</div>
+<span>{username}</span>
+<div className="photo-location">
+{location.city}, {location.country}
+</div>
+  </div>
+  );
 };
+
+
 
 export default PhotoListItem;
