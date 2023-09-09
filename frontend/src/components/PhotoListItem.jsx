7a8e4ae657e51f0971694f60f325fab62b5d49f6
ralph <ralphdcouto1987@gmail.com>
2023-09-09 00:08:51 -0400

    added styling to the photolistitem component using sass

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,34 +2,20 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
 const PhotoListItem = ({username, imageSource, id, location, profile}) => { 
   return (
-  <div className="photo-list-item">
-<div className="photo-username">
-<img src={imageSource} alt={`photo ${id}`} className="main-photo" />
-<img src={profile} alt={`${username} profile`} className="profile-photo" />
-</div>
-<span>{username}</span>
-<div className="photo-location">
-{location.city}, {location.country}
-</div>
-  </div>
+    <div className="photo-list__item">
+      <img src={imageSource} alt={`photo ${id}`} className="photo-list__image" />
+
+      <div className="photo-list__user-details">
+        <img src={profile} alt={`${username} profile`} className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <span>{username}</span>
+          <div className="photo-list__user-location">{location.city}, {location.country}</div>
+        </div>
+      </div>
+    </div>
   );
 };
-
-
 
 export default PhotoListItem;
