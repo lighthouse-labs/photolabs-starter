5136e30be960ceee8fbfa52181a81843ffd04fe3
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-11 18:01:04 -0400

    Feat: PhotoListItem starting prop Move starting sample data to app.js using sample data with props displaying basic photo with information using sample data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,30 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = ({ photoData }) => {
+  const { imageSource, location, username, profile } = photoData;
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <article className='Photo'>
+      <img 
+        src={imageSource}
+        alt={`Photo taken in ${location.city}, ${location.country}`}
+        className='Main-photo'
+      />
+      
+      <footer className='Photo-footer'>
+        <div className='Profile-section'>
+          <img 
+            src={profile}
+            alt={`${username}'s profile`}
+            className='Profile-pic'
+          />
+          <h2 className='Username'>{username}</h2>
+          <p>{location.city}, {location.country}</p>
+        </div>
+      </footer>
+    </article>
+  )
 };
 
 export default PhotoListItem;
