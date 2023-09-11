48541e3ba59431b004586997842d19fd0c45641c
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-11 17:59:56 -0400

    Feat: App.js sampleData add sample data to app.js to pass off as props to PhotoListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,20 @@ import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const sampleDataForPhotoListItem = {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  };
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem photoData ={sampleDataForPhotoListItem}/>
     </div>
   );
 };
