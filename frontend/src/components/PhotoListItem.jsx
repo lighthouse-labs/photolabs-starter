00e3462fd6c44d8494ebc1e8d733995e423d654b
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-12 16:14:34 -0400

    Add: PhotoListItem update component to match classnames for styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,31 +1,35 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/PhotoListItem.scss";
+import '../styles/PhotoListItem.scss';
 
 const PhotoListItem = ({ photoData }) => {
   const { imageSource, location, username, profile } = photoData;
 
   return (
-    <article className='Photo'>
-      <img 
+<article className='photo-list__item'>
+    <img
         src={imageSource}
         alt={`Photo taken in ${location.city}, ${location.country}`}
-        className='Main-photo'
-      />
-      
-      <footer className='Photo-footer'>
-        <div className='Profile-section'>
-          <img 
-            src={profile}
-            alt={`${username}'s profile`}
-            className='Profile-pic'
-          />
-          <h2 className='Username'>{username}</h2>
-          <p>{location.city}, {location.country}</p>
+        className='photo-list__image'
+    />
+
+    <footer className='Photo-footer'>
+        <div className='photo-list__user-details'>
+            <img
+                src={profile}
+                alt={`${username}'s profile`}
+                className='photo-list__user-profile'
+            />
+            <div>
+                <h2 className='photo-list__user-info'>{username}</h2>
+                <p className='photo-list__user-location'>
+                    {location.city}, {location.country}
+                </p>
+            </div>
         </div>
-      </footer>
-    </article>
-  )
+    </footer>
+</article>
+  );
 };
 
 export default PhotoListItem;
