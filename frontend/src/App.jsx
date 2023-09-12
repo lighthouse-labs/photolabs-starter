28b5d03db065930b37de3d8101448b60e8cd1fd5
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-12 16:13:23 -0400

    Feat: App.js creating aray to map PhotoListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,21 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const sampleDataForPhotoListItem = {
-    id: "1",
+    id: '1',
     location: {
-      city: "Montreal",
-      country: "Canada",
+      city: 'Montreal',
+      country: 'Canada'
     },
     imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    username: 'Joe Example',
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`
   };
 
   return (
     <div className="App">
-      <PhotoListItem photoData ={sampleDataForPhotoListItem}/>
+      {new Array(3).fill(sampleDataForPhotoListItem).map((photoData, index) => (
+        <PhotoListItem key={index} photoData={photoData} />
+      ))}
     </div>
   );
 };
