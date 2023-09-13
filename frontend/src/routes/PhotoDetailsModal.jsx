dce9ea5238100461117c735c537931a82abb9883
ralph <ralphdcouto1987@gmail.com>
2023-09-13 00:22:39 -0400

    trying to fix styling

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -13,19 +13,32 @@ const PhotoDetailsModal = ({ favoritedPhotos, updateToFavPhotoIds, photo, onClos
         <img src={closeSymbol} alt="close symbol" />
       </button>
       <div className="photo-details-modal__image-container">
+
       <PhotoFavButton 
         photoId={photo.id} 
         favoritedPhotos={favoritedPhotos} 
         updateToFavPhotoIds={updateToFavPhotoIds}
        />
+
       <img 
         src={photo.urls.regular} 
         alt={`photo ${photo.id}`} 
         className="photo-details-modal--image" 
       />
-       
+
+      <img 
+        src={photo.user.profile} 
+        alt={`${photo.user.username} profile`} 
+        className="photo-list__user-profile_modal" 
+      />
+      
+      <div className="photo-list__user-info_modal">
+          <span>{photo.user.username}</span>
+          <div className="photo-list__user-location_modal">{location.city}, {location.country}</div>
+        </div>
        </div>
-      <h2 className="photo-details-modal--header">Similar Photos</h2>
+       <div><h2 className="photo-details-modal--header">Similar Photos</h2></div>
+      
 
       <div className="photo-details-modal--images">
         <PhotoList photos={similarPhotos} />
