98e401abebd2713c04681360c55fc29269c609e6
Rina Inada <indrn@icloud.com>
2023-09-12 20:52:18 -0700

    Implement FavButton

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,19 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = (props) => {
+  const [selected, setSelected] = useState(false);
+  const toggleSelected = () => {
+    setSelected(!selected);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={toggleSelected}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={selected} displayAlert={false} />
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
