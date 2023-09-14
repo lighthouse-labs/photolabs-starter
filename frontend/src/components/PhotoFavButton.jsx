d59cf2275b3ba91836d1d464466079dc3cc12fc7
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-13 20:00:31 -0700

    create state and add click listener

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,20 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton({ displayAlert }) {
+  // const [isClicked, setIsClicked] = useState(selected);
+
+  const [selected, setSelected] = useState(false);
+
+  const clickHandler = () => {
+    setSelected(!selected);
+  };
+
+  console.log(selected);
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list__fav-icon-svg" onClick={clickHandler}>
+        <FavIcon selected={selected} />
       </div>
     </div>
   );
