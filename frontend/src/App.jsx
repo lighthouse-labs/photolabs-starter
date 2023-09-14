303c1d4373e4ee8b0d035f8434859c62295b1e3e
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-13 20:02:36 -0700

    Remove console.log

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
+import PhotoFavButton from 'components/PhotoFavButton';
 import './App.scss';
+
+const sampleDataForPhotoListItem = {
+  id: '1',
+  location: {
+    city: 'Montreal',
+    country: 'Canada'
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: 'Joe Example',
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`
+};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photos = new Array(3).fill(sampleDataForPhotoListItem);
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      {photos.map((photo, index) => {
+        return <PhotoListItem photoData={sampleDataForPhotoListItem} key={index} />;
+      })}
     </div>
   );
 };
