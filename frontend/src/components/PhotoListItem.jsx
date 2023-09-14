e7fdd3b6fa69a0704cb25566fffcdbee3f47bba5
ascotlan <antonio.k.scotland@gmail.com>
2023-09-14 17:37:40 -0400

    Apply Styling to the Photo Component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,16 +2,23 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = ({photoListData}) => {
+const PhotoListItem = ({ photoListData }) => {
   return (
-    <article>
-      <img src={photoListData.imageSource} />
-      <img src={photoListData.profile} />
-      <p>{photoListData.username}</p>
-      <p>
-        {photoListData.location.city},{" "}
-        {photoListData.location.country}
-      </p>
+    <article className="photo-list__item">
+      <div>
+        <img className="photo-list__image" src={photoListData.imageSource} />
+      </div>
+
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={photoListData.profile} />
+
+        <div className="photo-list__user-info">
+          <div>{photoListData.username}</div>
+          <div className="photo-list__user-location">
+            {photoListData.location.city}, {photoListData.location.country}
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
