ac8a6e40560aa6a5caf9c9d6a762e9f79abafb17
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-14 15:31:14 -0400

    Feat: Remove temp data in app and rendered photolist component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+// import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const sampleDataForPhotoListItem = {
-    id: '1',
-    location: {
-      city: 'Montreal',
-      country: 'Canada'
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: 'Joe Example',
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`
-  };
+ 
 
   return (
     <div className="App">
-      {new Array(3).fill(sampleDataForPhotoListItem).map((photoData, index) => (
+      {/* {new Array(3).fill(sampleDataForPhotoListItem).map((photoData, index) => (
         <PhotoListItem key={index} photoData={photoData} />
-      ))}
+      ))} */}
+      <PhotoList/>
     </div>
   );
 };
