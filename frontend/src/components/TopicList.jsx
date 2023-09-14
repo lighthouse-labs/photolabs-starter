fd3af5dbf2cd5ecbbc620695770e5245beb9c719
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-14 03:28:18 -0700

    map through topics list and render each topic

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,17 @@
-import React from "react";
+import React from 'react';
+import '../styles/TopicList.scss';
+import TopicListItem from './TopicListItem';
 
-import "../styles/TopicList.scss";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
+const TopicList = ({ topics }) => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {topics.map((topic) => {
+        return (
+          <p key={topic.id}>
+            <TopicListItem topic={topic} />
+          </p>
+        );
+      })}
     </div>
   );
 };
