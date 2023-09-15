57b79eb63841178bb1ec0e35c631a42fa443556c
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 15:11:34 -0400

    Feat: now passing mock data as props to children

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 // import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import HomeRoute from 'components/HomeRoute';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 // Note: Rendering a single component to build components in isolation
 const App = () => { 
  
 
   return (
     <div className="App">
-      <HomeRoute/>
+      <HomeRoute photos={photos} topics={topics} />
     </div>
   );
 };
