7f6bf23d628eb2aab29194de75b80f9350dd946a
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 14:40:52 -0400

    Refactor: App.jsx Remove old commented code and components. Feat: Add HomeRoute rendering which combines navbar and photolist components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 
 // import PhotoListItem from './components/PhotoListItem';
-import PhotoList from 'components/PhotoList';
 import './App.scss';
-import TopicList from 'components/TopicList';
-import TopNavigationBar from 'components/TopNavigationBar';
+import HomeRoute from 'components/HomeRoute';
 // Note: Rendering a single component to build components in isolation
 const App = () => { 
  
 
   return (
     <div className="App">
-    <TopNavigationBar />
-
-      {/* {new Array(3).fill(sampleDataForPhotoListItem).map((photoData, index) => (
-        <PhotoListItem key={index} photoData={photoData} />
-      ))} */}
-      {/*
-      <TopicList/>
-      <PhotoList/>
-    */}
+      <HomeRoute/>
     </div>
   );
 };
