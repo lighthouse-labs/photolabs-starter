d321fc3ac1288692714ecb4c62456e588d223f7e
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-14 21:12:22 -0700

    Add home route and clean up component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.scss';
-import PhotoList from 'components/PhotoList';
-import TopNavigationBar from 'components/TopNavigationBar';
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
+import HomeRoute from 'routes/HomeRoute';
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+ 
   return (
     <div className="App">
-      <TopNavigationBar topics={topics} />
-      <PhotoList photos={photos} />
+      <HomeRoute />
+      
     </div>
   );
 };
