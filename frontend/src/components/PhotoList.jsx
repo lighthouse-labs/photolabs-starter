10e35a1cee002ce75615ec8fe9732ec1b75cc9f6
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-14 21:18:28 -0700

    refactor component

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import '../styles/PhotoList.scss';
 import PhotoListItem from './PhotoListItem';
 
-const PhotoList = ({ photos }) => {
-  return (
-    <ul className="photo-list">
-      {photos.map((photo) => {
-        console.log(photo);
-        return (
-          <li key={photo.id}>
-            <PhotoListItem photoData={photo} />
-          </li>
-        );
-      })}
-    </ul>
-  );
+const PhotoList = ({ photos, selectedPhoto, isFavourite }) => {
+  const clickHandler = (photoId) => {
+    selectedPhoto(photoId);
+  };
+  const photoData = photos.map((photo) => {
+    // const isSelected = isFavourite(photo.id);
+    return (
+      <li key={photo.id}>
+        <PhotoListItem
+          photoData={photo}
+          selectedPhoto={selectedPhoto}
+          isFavourite={isFavourite(photo.id)}
+          onClick={() => {
+            clickHandler(photo.id);
+          }}
+        />
+      </li>
+    );
+  });
+
+  return <ul className="photo-list">{photoData}</ul>;
 };
 
 export default PhotoList;
