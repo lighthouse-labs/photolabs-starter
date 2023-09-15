533b74870cd43cb5213d50380795759a930da4b7
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 13:26:30 -0400

    Feat: TopicList and multiple fake data rendering using map and topicListItem component

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -23,7 +24,9 @@ const sampleDataForTopicList = [
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {sampleDataForTopicList.map((topicData) =>(
+        <TopicListItem key={topicData.id} title={topicData.title} link={topicData.slug} />
+      ))}
     </div>
   );
 };
