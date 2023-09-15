3f7d3cdd176ed175fa7e5487ef2f2601165007a1
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 15:13:54 -0400

    Feat: removed in file data using passed prop data instead

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,28 +3,10 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
+const TopicList = ({ topics }) => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topicData) =>(
+      {topics.map((topicData) =>(
         <TopicListItem key={topicData.id} title={topicData.title} link={topicData.slug} />
       ))}
     </div>
