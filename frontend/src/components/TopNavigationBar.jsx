8b61ef968dfd5aca90305e074f53c50dfeca23de
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 15:13:22 -0400

    Feat: passing mock data to children

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,11 +3,11 @@ import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = ({ topics }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList/>
+      <TopicList topics={topics}/>
       <FavBadge/>
     </div>
   )
