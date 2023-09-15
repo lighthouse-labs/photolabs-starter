611f2f301b01d853a4c13c84cf627045ff35a67f
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 14:21:46 -0400

    Feat: topNavBar update topnav to render topiclist and favbadge components

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import FavBadge from './FavBadge';
+import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList/>
+      <FavBadge/>
     </div>
   )
 }
