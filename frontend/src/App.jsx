29920b1d322c18ae16e75b89939673dde16c0833
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 14:14:39 -0400

    Feat: TopNavigationBar Add top nav bar component to app for development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,22 @@ import React from 'react';
 import PhotoList from 'components/PhotoList';
 import './App.scss';
 import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 // Note: Rendering a single component to build components in isolation
 const App = () => { 
  
 
   return (
     <div className="App">
+    <TopNavigationBar />
+
       {/* {new Array(3).fill(sampleDataForPhotoListItem).map((photoData, index) => (
         <PhotoListItem key={index} photoData={photoData} />
       ))} */}
+      {/*
       <TopicList/>
       <PhotoList/>
+    */}
     </div>
   );
 };
