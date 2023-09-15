3a900865a14cd88b4788006ee286c3d1371908aa
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-14 21:09:34 -0700

    move component rendering to home route and set global state

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
 import TopNavigationBar from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
 const HomeRoute = () => {
+  const [selected, setSelected] = useState([]);
+
+  const selectedPhoto = (photoDataId) => {
+    if (selected.includes(photoDataId)) {
+      const copySelected = [...selected].filter((photoId) => photoId !== photoDataId);
+      setSelected(copySelected);
+    } else {
+      setSelected((prevState) => [...prevState, photoDataId]);
+    }
+  };
+
+  const isFavourite = (photoDataId) => {
+    //should return true if the id exists in the selected array
+    return selected.includes(photoDataId);
+  };
+
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={topics} />
+      <PhotoList photos={photos} selectedPhoto={selectedPhoto} isFavourite={isFavourite} />
     </div>
   );
 };
