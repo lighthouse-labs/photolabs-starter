6d0d743bf1fdb8e0da7b079f0ddf7594d41cd63c
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-16 13:12:31 -0700

    pass props to set notifications

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -5,10 +5,10 @@ import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
-    <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+    <div className="fav-badge">
+      <FavIcon displayAlert={!!isFavPhotoExist} selected={true} />
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;
