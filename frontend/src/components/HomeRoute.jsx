e35eb361e2e33b7b1f7b552a662e0b8a09fee48b
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 20:58:39 -0400

    Feat: homeroute add setState using function toggleFavorite which creates an empty array and populates it with photo ids then passes that info to its children

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopNavigationBar from './TopNavigationBar';
 import PhotoList from './PhotoList';
 import '../styles/HomeRoute.scss'
 
 const HomeRoute = ({ photos, topics}) => {
+  const [favoritedPhotoIds, setFavoritedPhotoIds] = useState([])
+
+  const toggleFavorite = (photoId) => {
+    if (favoritedPhotoIds.includes(photoId)) {
+      const newFavoritedPhotoIds = favoritedPhotoIds.filter(id => id !== photoId);
+      setFavoritedPhotoIds(newFavoritedPhotoIds);
+      console.log("Updated favoritedPhotoIds:", newFavoritedPhotoIds);
+    } else {
+      const newFavoritedPhotoIds = [...favoritedPhotoIds, photoId];
+      setFavoritedPhotoIds(newFavoritedPhotoIds);
+      console.log("Updated favoritedPhotoIds:", newFavoritedPhotoIds);
+    }
+  };
+
   return (
     <div className="home-route">
-      <TopNavigationBar topics={topics}/>
-      <PhotoList photos={photos}/>
+      <TopNavigationBar 
+        topics={topics}
+        favoritedPhotoIds={favoritedPhotoIds}/>
+      <PhotoList 
+        photos={photos} 
+        favoritedPhotoIds={favoritedPhotoIds} 
+        toggleFavorite={toggleFavorite}/>
    </div>
   )
 }
