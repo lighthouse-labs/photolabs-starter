583779acf066c53400df27b4a76f047bd54d5bb0
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-16 13:31:29 -0400

    Feat: App.js toggleModal add two states isModalOpen & selectedPhoto isModalOpen switches between true and false based on user clicking image to render modal SelectedPhoto passes photo data to modal component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import HomeRoute from 'components/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 // Note: Rendering a single component to build components in isolation
 const App = () => { 
- 
+ const [isModalOpen, setIsModalOpen] = useState(false)
+ const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+ const toggleModal = (photoData = null) => {
+  setIsModalOpen(!isModalOpen)
+  setSelectedPhoto(photoData)
+  console.log(photoData);
+ };
 
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} />
+      <HomeRoute 
+      photos={photos} 
+      topics={topics}
+      toggleModal={toggleModal} />
+      {isModalOpen && <PhotoDetailsModal photoData={selectedPhoto} toggleModal={toggleModal}/>}
     </div>
   );
 };
