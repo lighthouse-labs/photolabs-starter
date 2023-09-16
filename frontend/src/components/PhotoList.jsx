ecb70cb01b18d03f49415ef2e327c735c523febf
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-16 13:34:55 -0400

    Feat: pass toggleModal to children

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
-const PhotoList = ({ photos, favoritedPhotoIds, toggleFavorite }) => {
+const PhotoList = ({ photos, favoritedPhotoIds, toggleFavorite, toggleModal }) => {
   return (
     <ul className="photo-list">
       {photos.map((photoData) =>(
@@ -10,7 +10,8 @@ const PhotoList = ({ photos, favoritedPhotoIds, toggleFavorite }) => {
           key={photoData.id} 
           photoData={photoData}
           isFavorited={favoritedPhotoIds.includes(photoData.id)}
-          toggleFavorite={toggleFavorite} />
+          toggleFavorite={toggleFavorite}
+          toggleModal={toggleModal} />
       ))}
     </ul>
   );
