1174eb618feff95ef3ebe5869c8d917ab117bbfd
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-15 21:06:46 -0400

    Feat: TopDavigationBar passes the favorite array information for use with rendering notification on nav bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,12 +3,12 @@ import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = ({ topics }) => {
+const TopNavigation = ({ topics, favoritedPhotoIds }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics}/>
-      <FavBadge/>
+      <FavBadge favoritedPhotoIds={favoritedPhotoIds}/>
     </div>
   )
 }
