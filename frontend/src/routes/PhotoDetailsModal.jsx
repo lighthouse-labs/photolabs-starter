4029f34590c6ca7bbfc133a52e1ffc994b4d6f88
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-16 13:34:14 -0400

    Feat: PhotoDetailsModal add onclick listener to close modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ toggleModal, photoData }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={toggleModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
