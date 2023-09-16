20feca28f5b0fd08f80ac177f7e0ae51a1021e89
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-09-16 13:17:58 -0700

    pass props from homeroutes

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,12 +4,13 @@ import '../styles/TopNavigationBar.scss';
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 
-const TopNavigationBar = ({ topics }) => {
+const TopNavigationBar = ({ topics, isFavPhotoExist }) => {
+  console.log('isFavPhotoExist :', isFavPhotoExist);
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} />
-      <FavBadge />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
   );
 };
