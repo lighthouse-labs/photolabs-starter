e0a2503d8e3674920af279efbed33f67feac6404
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-17 13:24:36 -0400

    Feat: PhotoDetailModal renders larger image using photodatas information renders PhotoList component with similarPhotosArray which is transformed from similar photos object

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,14 +2,51 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from 'components/PhotoList';
+import PhotoFavButton from 'components/PhotoFavButton';
 
-const PhotoDetailsModal = ({ toggleModal, photoData }) => {
+const PhotoDetailsModal = ({ toggleModal, photoData, favoritedPhotoIds, toggleFavorite }) => {
+  const { urls, location, user, similar_photos } = photoData;
+  const largerImageSource = urls.full;
+  const { username, profile } = user;
+  const similarPhotosArray = Object.values(similar_photos);
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={toggleModal}>
+    <button className="photo-details-modal__close-button" onClick={toggleModal}>
         <img src={closeSymbol} alt="close symbol" />
-      </button>
+    </button>
+    <div className='photo-details-modal__images'>
+    <PhotoFavButton 
+  isFavorited={favoritedPhotoIds.includes(photoData.id)}
+  toggleFavorite={() => toggleFavorite(photoData.id)} 
+/>
+    <img
+        src={largerImageSource}
+        alt={`Large version of selected photo`}
+        className='photo-details-modal__image'
+    />
+    <div className="photo-details-modal__photographer-details">
+        <img
+            src={profile}
+            alt={`${username}'s profile`}
+            className='photo-list__user-profile' 
+        />
+        <div>
+            <h2 className='photo-list__user-info'>{username}</h2>
+            <p className='photo-list__user-location'>
+                {location.city}, {location.country}
+            </p>
+        </div>
     </div>
+    </div>
+    <h3 className="photo-details-modal__images">Similar Photos</h3>
+    <PhotoList 
+      photos={similarPhotosArray} 
+      toggleModal={toggleModal}
+      favoritedPhotoIds={favoritedPhotoIds}
+      toggleFavorite={toggleFavorite} />   
+</div>
   )
 };
 
