f8641a3ceb009c9d192941503890a4dd52c9a0a2
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-09-21 12:40:24 -0600

    added changes

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
+// const sampleDataForTopicListItem = {
+//   id: "1",
+//   slug: "topic-1",
+//   label: "Nature",
+// };
+
 
 const TopicListItem = (props) => {
+
+
+
   return (
     <li
       className="topic-list__item"
       onClick={() => {
         props.handleImageFetch(props.topicId);
+
       }}
     >
       <span>{props.topicTitle}</span>
