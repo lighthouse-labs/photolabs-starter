7027b438be93092977e8f25d878245455d4e9172
RomelDonovan <romeldonovan@live.ca>
2023-10-04 14:56:06 -0400

    Move sampleDataForPhotoListItem into your App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
