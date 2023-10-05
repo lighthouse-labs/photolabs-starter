320861defaa7bad69fe1080b03d94126b9eb0c5c
RomelDonovan <romeldonovan@live.ca>
2023-10-05 13:13:06 -0400

    Refactor handeClick

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -5,8 +5,7 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
   const [selected, setSelected] = useState(false);
-
-  const handleClick = () => setSelected(false ? selected : !selected);
+  const handleClick = () => setSelected(!selected);
 
   return (
     <div className="photo-list__fav-icon" onClick={handleClick}>
