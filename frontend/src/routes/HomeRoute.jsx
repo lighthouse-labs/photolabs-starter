35a5229102da5480bf2270d3d0b96328b79858b4
Brett Roberts <brett.roberts@live.ca>
2023-10-05 16:12:24 -0600

    Create home route component

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../styles/HomeRoute.scss';
+import TopNavigation from 'components/TopNavigationBar';
+import TopicList from 'components/TopicList';
+import FavBadge from 'components/FavBadge';
+import PhotoList from 'components/PhotoList';
 
 const HomeRoute = () => {
+  const [selectedCount, setSelectedCount] = useState(0)
+  
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <div className='top-nav-bar'>
+        <TopNavigation />
+        <TopicList/>
+        <FavBadge isFavPhotoExist={selectedCount}/>
+      </div>
+      <PhotoList setSelectedCount={setSelectedCount}/>
     </div>
   );
 };
