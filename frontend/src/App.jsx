4e9951fed930d5c821acbb91d215ecaab3e04b2e
Brett Roberts <brett.roberts@live.ca>
2023-10-05 16:07:05 -0600

    Add home route logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+ import PhotoList from './components/PhotoList';
+ import './App.scss';
+import TopicListItem from 'components/TopicListItem';
+import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
+import FavBadge from 'components/FavBadge';
+import HomeRoute from 'routes/HomeRoute';
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
   return (
-    <div className="App">
-      <PhotoListItem/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/home" element={<HomeRoute />} />
+      </Routes>
+      
+    </Router>
   );
-};
+}
 
 export default App;
