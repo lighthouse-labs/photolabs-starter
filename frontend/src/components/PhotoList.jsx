4f4327c06b6889564d64e57e6de475290bced23b
Brett Roberts <brett.roberts@live.ca>
2023-10-05 16:10:13 -0600

    Remove sample data - import mocks data

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,65 +1,32 @@
 import React from "react";
-
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
+import photos from "mocks/photos";
 
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
+const PhotoList = (props) => {
+  console.log("PhotoList Props: ", props);
 
-const PhotoList = () => {
+  const renderPhotoListItems = () => {
+    return photos.map((photo) => {
+      const { id, location, urls: { regular: imageSource }, user: { name, profile }} = photo;
+
+      return (
+        <li key={id}>
+          <PhotoListItem 
+            imageSource={imageSource} 
+            name={name} 
+            profile={profile} 
+            location={location}
+            setSelectedCount={props.setSelectedCount}
+          />
+        </li>
+      );
+    });
+  }
+
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {renderPhotoListItems()}
     </ul>
   );
 };
