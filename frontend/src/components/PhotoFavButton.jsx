f3ed6c4edd07ae5502f3a9919697b9d522323fdd
Brett Roberts <brett.roberts@live.ca>
2023-10-05 16:08:55 -0600

    Add logic to track global state of favourited photos

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,28 @@
 import React, { useCallback, useState } from 'react';
-
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+
+function PhotoFavButton(props) {
+  console.log("FavButton Props: ", props);
+
+  const [localSelected, setLocalSelected] = useState('')
+
+  function handleClick() {
+    console.log('clicked');
+
+    
+    setLocalSelected((prev) => prev === '' ? 'liked' : '')
+
+    localSelected === '' ? 
+    props.setSelectedCount((prev) => prev + 1) : 
+    props.setSelectedCount((prev) => prev - 1);
+  }
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon"  onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={localSelected} />
       </div>
     </div>
   );
