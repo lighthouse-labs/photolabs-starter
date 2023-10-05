4c61a58bcd14643e90a2d0339495f0be5d4712d3
RomelDonovan <romeldonovan@live.ca>
2023-10-05 11:17:53 -0400

    A user can like a photo

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,11 +4,14 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [selected, setSelected] = useState(false);
+
+  const handleClick = () => setSelected(false ? selected : !selected);
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon />
-        {/* Insert React */}
+      <FavIcon selected={selected} />
       </div>
     </div>
   );
