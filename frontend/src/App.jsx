04677e9765f5bf812cca6683a165b68049e5af65
RomelDonovan <romeldonovan@live.ca>
2023-10-05 14:44:02 -0400

    Remove PhotoListItem and add PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,13 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const photos = Array(3).fill(sampleDataForPhotoListItem);
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {photos.map((photo, id) => (
-        <PhotoListItem
-          key={id}
-          id={sampleDataForPhotoListItem.id}
-          location={sampleDataForPhotoListItem.location}
-          imageSource={sampleDataForPhotoListItem.imageSource}
-          username={sampleDataForPhotoListItem.username}
-          profile={sampleDataForPhotoListItem.profile}
-        />
-      ))}
+     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+     <PhotoList/>
     </div>
   );
 };
