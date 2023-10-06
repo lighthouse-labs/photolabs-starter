c7958330d4e568634a2440fc35d37d0ba7f760e6
Brett Roberts <brett.roberts@live.ca>
2023-10-05 23:54:10 -0600

    refactor favourites state logic

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,17 +5,19 @@ import TopicList from 'components/TopicList';
 import FavBadge from 'components/FavBadge';
 import PhotoList from 'components/PhotoList';
 
-const HomeRoute = () => {
+
+const HomeRoute = (props) => {
+  console.log("HomeRoute render");
+
   const [favourites, setFavourites] = useState([]);
+  console.log("favourites:", favourites);
 
-const toggleFavourite = function(id) {
-  if (favourites.includes(id)) {
-    setFavourites(prevFavourites => prevFavourites.filter(favId => favId !== id));
-  } else {
-    setFavourites(prevFavourites => [...prevFavourites, id]);
-  }
+  const toggleFavourite = id => {
+    setFavourites(prevFavourites => 
+        prevFavourites.includes(id) ? prevFavourites.filter(favId => favId !=id)
+        : [...prevFavourites, id]
+    );
 };
-
 function isFavPhotoExist() {
   if (favourites.length === 0) {
     return false;
@@ -23,8 +25,6 @@ function isFavPhotoExist() {
     return true;
   }
 }
-
-  
   return (
     <div className="home-route">
       <div className='top-nav-bar'>
@@ -32,7 +32,7 @@ function isFavPhotoExist() {
         <TopicList/>
         <FavBadge isFavPhotoExist={isFavPhotoExist()}/>
       </div>
-      <PhotoList favourites={favourites} toggleFavourite={toggleFavourite}/>
+      <PhotoList setShowModal={props.setShowModal} favourites={favourites} toggleFavourite={toggleFavourite} setSelectedImage={props.setSelectedImage}/>
     </div>
   );
 };
