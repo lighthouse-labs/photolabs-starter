148dc9162251decc95f078be54350aab148e5b56
Brett Roberts <brett.roberts@live.ca>
2023-10-05 23:49:43 -0600

    Add logic to show modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
- import PhotoList from './components/PhotoList';
  import './App.scss';
-import TopicListItem from 'components/TopicListItem';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
-import FavBadge from 'components/FavBadge';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState({})
+  console.log("Selected ID: ", selectedImage);
+  
   return (
     <Router>
       <Routes>
-        <Route exact path="/home" element={<HomeRoute />} />
+        <Route exact path="/home" element={<HomeRoute setShowModal={setShowModal} setSelectedImage={setSelectedImage}/>} />
       </Routes>
+
+      {showModal && <PhotoDetailsModal 
+      setShowModal={setShowModal}
+      id={selectedImage.id}
+      full={selectedImage.full}
+      regular={selectedImage.regular}
+      name={selectedImage.name}
+      location={selectedImage.location}
+      profile={selectedImage.profile}
+      favourites={selectedImage.favourites}
+      toggleFavourite={selectedImage.toggleFavourite}
+      similar_photos={selectedImage.similar_photos}
       
+  
+      />}
     </Router>
   );
 }
