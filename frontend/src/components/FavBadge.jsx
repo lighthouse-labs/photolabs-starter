af38d0e67cd856749b720cd6c72118d903d1b4c2
Brett Roberts <brett.roberts@live.ca>
2023-10-09 19:34:34 -0600

    feat: add count to favourites badge

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,10 +3,13 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = ({ isFavPhotoExist, favourites }) => {
   // console.log("isFavPhotoExist: ", isFavPhotoExist);
+  const count = favourites.length;
+
     return (
     <div className='fav-badge'>
+      <div className='favourites-count'>{count > 0 && count}</div>
       <FavIcon displayAlert={isFavPhotoExist} selected={isFavPhotoExist} />
     </div>
   ) 
