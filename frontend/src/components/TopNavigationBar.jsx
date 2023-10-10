62a911e760ff656012f961cbdbf7159416f4802d
Brett Roberts <brett.roberts@live.ca>
2023-10-09 20:09:06 -0600

    Add onClick to display all photos on logo click

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span onClick={props.homePhotos} className="top-nav-bar__logo">PhotoLabs</span>
     </div>
   )
 }
