5a91adced7888079b8ef9f991128278c4ca38ee7
Angela Zhou <a03zhou@gmail.com>
2023-10-13 21:16:49 -0400

    add photoItemList sass styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,11 +4,15 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <div className="photoListItem">
-      <img className="image" src={props.imageSource}></img>
-      <img className="profile" src={props.profile}></img>
-      <p className="username"> {props.username} </p>
-      <p className="location"> {props.city} {props.country} </p>
+    <div className="photoListItem photo-list__item">
+      <img className="image photo-list__image" src={props.imageSource}></img>
+      <div className="photo-list__user-details">
+        <img className="profile photo-list__user-profile" src={props.profile}></img>
+        <div>
+          <h1 className="username photo-list__user-info"> {props.username} </h1>
+          <p className="location photo-list__user-info photo-list__user-location"> {props.city}, {props.country} </p>
+        </div>
+      </div>
     </div>
   )
 };
