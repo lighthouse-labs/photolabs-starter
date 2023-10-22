f1419e6c8af20397be7f19d67ef6e83414913056
Terrence L <terrence.liu@alumni.ubc.ca>
2023-10-22 16:34:23 -0700

    Add base for posting photos

--- backend/src/routes/photos.js
+++ backend/src/routes/photos.js
@@ -124,5 +124,16 @@ module.exports = db => {
     });
   });
 
+  // router.post("/photos/new", (request, response) => {
+  //   const protocol = request.protocol;
+  //   const host = request.hostname;
+  //   const port = process.env.PORT || 8001;
+  //   const serverUrl = `${protocol}://${host}:${port}`;
+  //   db.query(`
+  //   `).then(({ rows }) => {
+  //     response.json(rows[0].photo_data);
+  //   });
+  // });
+
   return router;
 };
