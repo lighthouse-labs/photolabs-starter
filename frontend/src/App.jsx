e22cef8bd7e93a63251b6c9b0849f658455812e2
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-12-04 02:13:04 -0500

    update App.js according to useApplicationData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,56 +14,20 @@ import topicData from "./mocks/topics";
 import { useApplicationData } from "./hooks/useApplicationData";
 import * as photoHelpers from "./helpers/photolabsHelpers";
 
-// import HomeRoute from "routes/HomeRoute";
-
 const App = () => {
-  const [photos, setPhotos] = useState(photoData);
-  const [topics, setTopics] = useState(topicData);
+  const {
+    photos,
+    topics,
+    toggleFavoritePhoto,
+    isPhotoFavorite,
+    checkFavorites,
+    photoClickHandler,
+    openModal,
+    isOpenModal,
+    clickedPhoto,
+    closeModal,
+  } = useApplicationData();
 
-  // State for modal and clicked photo
-  const [openModal, setOpenModal] = useState(false);
-  const [clickedPhoto, setClickedPhoto] = useState(null);
-
-  // Close modal
-  const closeModal = () => {
-    setOpenModal(false); //Modal window is closed
-    setClickedPhoto(null);
-  };
-
-  // Handler when photo is clicked
-  const photoClickHandler = (photoId) => {
-    setClickedPhoto(photoId); //clicked photo
-    setOpenModal(true); //Modal window is opened
-  };
-
-  const isPhotoFavorite = (photoId) => {
-    const photo = photos.find((photo) => Number(photo.id) === Number(photoId));
-    if (photo) {
-      // if ("isFavorite" in photo) {
-      if (photo.isFavorite) {
-        return photo.isFavorite;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  };
-  // return photo ? photo.isFavorite : false;
-
-  const checkFavorites = () => {
-    return photos.some((el) => el.isFavorite === true);
-  };
-
-  const toggleFavoritePhoto = (photoId) => {
-    setPhotos((prevData) =>
-      prevData.map((photo) =>
-        photo.id === photoId
-          ? { ...photo, isFavorite: !photo.isFavorite }
-          : photo
-      )
-    );
-  };
   return (
     <div className="App">
       <HomeRoute
@@ -74,52 +38,17 @@ const App = () => {
         photoClickHandler={photoClickHandler}
         checkFavorites={checkFavorites}
       />
-      {openModal && (
+      {isOpenModal && (
         <PhotoDetailsModal
           photos={photos}
           closeModal={closeModal}
           isPhotoFavorite={isPhotoFavorite}
           clickedPhoto={clickedPhoto}
+          openModal={openModal}
         />
       )}
     </div>
   );
+  // ...
 };
-// _____________________________________________________________________With useReducer
-// const App = () => {
-//   const {
-//     photos,
-//     topics,
-//     modal,
-//     setModal,
-//     toggleFavoritePhoto,
-//     isPhotoFavorite,
-//     closeModal,
-//     checkFavorites,
-//     photoClickHandler,
-//   } = useApplicationData();
-
-//   return (
-//     <div className="App">
-//       <HomeRoute
-//         photos={photos}
-//         topics={topics}
-//         isPhotoFavorite={isPhotoFavorite}
-//         toggleFavoritePhoto={toggleFavoritePhoto}
-//         photoClickHandler={photoClickHandler}
-//         checkFavorites={checkFavorites}
-//       />
-//       {modal.isOpenModal && (
-//         <PhotoDetailsModal
-//           photos={photos}
-//           closeModal={closeModal}
-//           isPhotoFavorite={isPhotoFavorite}
-//           clickedPhoto={modal.clickedPhoto}
-//           isOpenModal={() => setModal({ type: "CLOSE_MODAL" })}
-//         />
-//       )}
-//     </div>
-//   );
-// };
-
 export default App;
