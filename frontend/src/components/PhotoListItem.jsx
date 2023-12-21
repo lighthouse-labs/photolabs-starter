9695aa6c8d7f31ba6909a621a012be68d84cf13f
Nathan <nathanrmsmith@gmail.com>
2023-12-21 12:37:36 -0500

    Built initial PhotoListItem component with map and array of obj as db

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,27 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({dataList}) => {
+  return (
+  <div>
+    {dataList.map((data) => (
+      <section key={data.id} className="photo-list__item">
+        <img className="photo-list__image" src={data.imageSource}/>
+          <div className="photo-list__user-details">
+            <img className="photo-list__user-profile" src={data.profile}/>
+            <div>
+              <div>
+                <span className="photo-list__user-info">{data.username}</span>
+              </div>
+              <div>
+                <span className="photo-list__user-location photo-list__user-info">{data.location.city}, {data.location.country}</span>
+              </div>
+            </div>
+          </div>
+      </section> 
+    ))}
+  </div>
+  )
 };
 
 export default PhotoListItem;
