f25231385fdad2930a5c754245b34975c6b28f33
Vhkan <vhkan@yahoo.com>
2024-02-05 12:26:04 -0700

    Reafctor the useApplicationData file logic to use useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,29 +1,81 @@
 import { useState } from 'react';
+import { useReducer } from 'react';
 
+//Add level actions
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
 
-export const useApplicationData = () => {
-
-  //Favotires
-  const [favorite, setFavorite] = useState([]);
-
-  const toggleFavorite = (id) => {
-    setFavorite((presentFavorites) => {
-      if (presentFavorites.includes(id)) {
-        return presentFavorites.filter(favoriteId => favoriteId !== id);
-      } else {
-        return [...presentFavorites, id]
-      }
-    })
+// Refactoring using useState =>
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return { ...state, favorite: [...state.favorite, action.payload] };
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return {
+        ...state, favorite: state.favorite.filter(id => id !== action.payload)
+      };
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return { ...state, displayModal: action.payload };
+    default:
+      return state;
   }
-
-  //DisplayModal
-  const [displayModal, setDisplayModal] = useState(false);
-
-  const updateDisplayModal = (props) => {
-    const modalProps = { ...props, modalState: true };
-    displayModal ? setDisplayModal(false) : setDisplayModal(modalProps);
-  }
-
-  return { favorite, toggleFavorite, displayModal, updateDisplayModal };
 };
 
+export const useApplicationData = () => {
+  const initialState = {
+    favorite: [],
+    displayModal: false
+  };
+
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const toggleFavorite = id => {
+    const actionType = state.favorite.includes(id);
+    if (actionType) {
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: id })
+    } else {
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: id })
+    }
+  };
+
+
+  const updateDisplayModal = (props) => {
+    const modalState = state.displayModal ? false : props;
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: modalState });
+  };
+  return { ...state, toggleFavorite, updateDisplayModal }
+};
+
+
+// export const useApplicationData = () => {
+
+//   //Favotires
+//   const [favorite, setFavorite] = useState([]);
+
+//   const toggleFavorite = (id) => {
+//     setFavorite((presentFavorites) => {
+//       if (presentFavorites.includes(id)) {
+//         return presentFavorites.filter(favoriteId => favoriteId !== id);
+//       } else {
+//         return [...presentFavorites, id]
+//       }
+//     })
+//   }
+
+//   //DisplayModal
+//   const [displayModal, setDisplayModal] = useState(false);
+
+//   const updateDisplayModal = (props) => {
+//     const modalProps = { ...props, modalState: true };
+//     displayModal ? setDisplayModal(false) : setDisplayModal(modalProps);
+//   }
+
+//   return { favorite, toggleFavorite, displayModal, updateDisplayModal };
+// };
